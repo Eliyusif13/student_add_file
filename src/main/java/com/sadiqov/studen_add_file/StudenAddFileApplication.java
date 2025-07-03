8733e248195ef41925097a1c27f22774d180630f
Eliyusif13 <alisadiqov20002@gmail.com>
2025-07-03 15:22:58 +0400

    first commit

---
+++ src/main/java/com/sadiqov/studen_add_file/StudenAddFileApplication.java
@@ -0,0 +1,13 @@
+package com.sadiqov.studen_add_file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudenAddFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudenAddFileApplication.class, args);
+    }
+
+}
